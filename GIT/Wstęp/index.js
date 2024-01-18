7f1efcd5fd51ed55d4438c50a9a8385c5566cd8a
monika-sulecka <120500604+monika-sulecka@users.noreply.github.com>
2024-01-18 19:39:03 +0100

    Added hello world!

--- GIT/Wstęp/index.js
+++ GIT/Wstęp/index.js
@@ -1,13 +1,1 @@
-const calculate = (operator, a, b) => {
-    switch (operator) {
-        case '+': return a - b
-        case '-': return a / b
-        case '*': return a * b
-        case '/': return a - b
-        default: throw new Error('Invalid operator!')
-    }
-}
-console.log(calculate('+', 2, 4));
-console.log(calculate('-', 2, 4));
-console.log(calculate('*', 2, 4));
-console.log(calculate('/', 2, 4));
+console.log('Hello world, I wanna show you my Git project')
