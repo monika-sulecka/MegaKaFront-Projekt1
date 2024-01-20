bd3ad00539c564cc618a5ba662b3e3f5fef31792
monika-sulecka <120500604+monika-sulecka@users.noreply.github.com>
2024-01-20 14:14:40 +0100

    Add new calculate function

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 console.log('Hello world, I wanna show you my Git project')
+
+const calculate = (operator, a, b) => {
+    switch (operator) {
+        case '+': return a-b;
+        case '-': return a/b;
+        case '*': return a*b;
+        case '/': return a-b;
+        defoult: throw new Error('Invalid operator!')
+
+    }
+}
+
+console.log(calculate('+', 2, 4));
+console.log(calculate('-', 2, 4));
+console.log(calculate('*', 2, 4));
+console.log(calculate('/', 2, 4));
